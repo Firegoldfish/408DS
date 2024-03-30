d71200674c64309d2b8914cf78cec4a5ee3e2961
Firegoldfish <firegoldfish@outlook.com>
2024-03-30 20:13:23 +0800

    遍历二叉树

--- ch5.cpp
+++ ch5.cpp
@@ -7,6 +7,11 @@ typedef struct BiTNode //链式存储二叉树
     struct BiTNode *lchild, *rchild;
 }BiTNode, *BiTree;
 
+typedef struct SqTree //顺序存储二叉树
+{
+    char data[100];
+}SqTree;
+
 int GetTogetherFather(SqTree T,int i, int j){
     if (T[i]!='#'&&T[j]!='#') {
         while (i!=j){
@@ -15,4 +20,35 @@ int GetTogetherFather(SqTree T,int i, int j){
         }
         return T[i];
     }
+}
+
+void visit(BiTree T){
+    cout<<T->data<<endl;
+}
+
+void PreOrder(BiTree T){ //前序遍历
+    if (T!=NULL)
+    {
+        visit(T);
+        PreOrder(T->lchild);
+        PreOrder(T->rchild);
+    }
+}
+
+void InOrder(BiTree T){
+    if(T!=NULL){
+        InOrder(T->lchild);
+        visit(T);
+        InOrder(T->rchild);
+    }
+}
+
+void PostOrder(BiTree T){ //后序遍历
+    if (T!=NULL)
+    {
+        PostOrder(T->lchild);
+        PostOrder(T->rchild);
+        visit(T);
+    }
+    
 }
